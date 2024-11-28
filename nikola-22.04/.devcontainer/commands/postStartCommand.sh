cbf4c040457683445f0ea276fe4730201cadccb0
Ralph Hempel <rhempel@hempeldesigngroup.com>
2024-11-28 12:38:14 -0500

    Align contents of the Nikola variants

---
+++ nikola-22.04/.devcontainer/commands/postStartCommand.sh
@@ -0,0 +1,21 @@
+this_user=$1
+
+# Update the .gitconfig file by combining the original .gitconfig
+# created when the devcontainer was created with the current
+# users's .gitconfig that was mounted at ~/.gitconfig.host
+#
+cd ~
+
+if [ ! -f .gitconfig.original ]; then
+  echo "copying .gitconfig" >> ~/postStart
+  cp .gitconfig .gitconfig.original
+fi;
+
+if [ ! -f .gitconfig ]; then
+  echo "No .gitconfig" >> ~/postStart
+  cp .gitconfig .gitconfig.original
+fi;
+
+# Now create the updated .gitconfig
+#
+cat .gitconfig.original .gitconfig.host > .gitconfig

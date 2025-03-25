ff307b879751eced347244790e11027f68b883a0
Ralph Hempel <rhempel@hempeldesigngroup.com>
2025-03-25 16:43:50 -0400

    Clean up the postXXX commands to avoid problems with .gitconfig

--- adaptabuild-example-22.04/.devcontainer/commands/postStartCommand.sh
+++ adaptabuild-example-22.04/.devcontainer/commands/postStartCommand.sh
@@ -10,22 +10,25 @@ if [ ! -d adaptabuild-example ]; then
   git submodule update --init --recursive
 fi;
 
-# Update the .gitconfig file by combining the original .gitconfig
-# created when the devcontainer was created with the current
-# users's .gitconfig that was mounted at ~/.gitconfig.host
-#
-cd ~
+# The section on custom gitconfig should be handled with an include see
+# the container_base_images repo for notes
 
-if [ ! -f .gitconfig.original ]; then
-  echo "copying .gitconfig" >> ~/postStart
-  cp .gitconfig .gitconfig.original
-fi;
-
-if [ ! -f .gitconfig ]; then
-  echo "No .gitconfig" >> ~/postStart
-  cp .gitconfig .gitconfig.original
-fi;
-
-# Now create the updated .gitconfig
-#
-cat .gitconfig.original .gitconfig.host > .gitconfig
+# # Update the .gitconfig file by combining the original .gitconfig
+# # created when the devcontainer was created with the current
+# # users's .gitconfig that was mounted at ~/.gitconfig.host
+# #
+# cd ~
+# 
+# if [ ! -f .gitconfig.original ]; then
+#   echo "copying .gitconfig" >> ~/postStart
+#   cp .gitconfig .gitconfig.original
+# fi;
+# 
+# if [ ! -f .gitconfig ]; then
+#   echo "No .gitconfig" >> ~/postStart
+#   cp .gitconfig .gitconfig.original
+# fi;
+# 
+# # Now create the updated .gitconfig
+# #
+# cat .gitconfig.original .gitconfig.host > .gitconfig
